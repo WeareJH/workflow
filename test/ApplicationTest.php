c7f27e7ff7851869a1c6bc92e1d5ba0c8d3b30b2
Aydin Hassan <aydin@hotmail.co.uk>
2017-02-10 11:51:24 +0100

    Fix test

--- test/ApplicationTest.php
+++ test/ApplicationTest.php
@@ -3,12 +3,8 @@
 use Jh\Workflow\Application;
 use Jh\Workflow\Command\Magento;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\ExceptionInterface;
-use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class ApplicationTest extends TestCase
 {
@@ -26,24 +22,11 @@ class ApplicationTest extends TestCase
             }
         });
 
-        $fallback = $this->getMockBuilder(Magento::class)
-            ->setConstructorArgs([new \Jh\Workflow\ProcessFactory])
-            ->setMethods(['run'])
-            ->getMock();
-
-        $fallback
-            ->expects($this->once())
-            ->method('run')
-            ->with(static::isInstanceOf(InputInterface::class), static::isInstanceOf(OutputInterface::class))
-            ->willReturnCallback(function (InputInterface $input, $output) use ($fallback) {
-                try {
-                    $input->bind($fallback->getDefinition());
-                } catch (ExceptionInterface $e) {
-                }
-
-                static::assertEquals($input->getArguments(), ['cmd' => 'magento']);
-                return 0;
-            });
+        $fallback = new Magento(new \Jh\Workflow\ProcessFactory());
+        $fallback->setCode(function (InputInterface $input) {
+            static::assertTrue($input->hasArgument('cmd'));
+            static::assertEquals($input->getArgument('cmd'), 'some-command-that-does-not-exist');
+        });
 
         $app->add($fallback);
 
