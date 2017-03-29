a9bc388c04ae20ec76e6b3ac5d191d2925f5fafa
Aydin Hassan <aydin@hotmail.co.uk>
2017-03-29 15:44:40 +0200

    di compile command

--- app/config.php
+++ app/config.php
@@ -23,6 +23,7 @@ return [
         $app->add($c->get(Command\MagentoFullInstall::class));
         $app->add($c->get(Command\MagentoInstall::class));
         $app->add($c->get(Command\MagentoConfigure::class));
+        $app->add($c->get(Command\MagentoCompile::class));
         $app->add($c->get(Command\Pull::class));
         $app->add($c->get(Command\Push::class));
         $app->add($c->get(Command\Watch::class));
