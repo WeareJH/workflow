ffd712b744167c7a4ebb47405c51db1f78303fe0
Aydin Hassan <aydin@hotmail.co.uk>
2017-07-11 12:00:46 +0200

    User and group owner constraint

---
+++ src/Test/Constraint/FileGroupAndOwnerInContainer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Jh\Workflow\Test\Constraint;
+
+use PHPUnit\Framework\Constraint\Constraint;
+
+/**
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+class FileGroupAndOwnerInContainer extends Constraint
+{
+    /**
+     * @var string
+     */
+    private $container;
+
+    /**
+     * @var string
+     */
+    private $group;
+    /**
+     * @var string
+     */
+    private $user;
+
+    public function __construct(string $container, string $group, string $user)
+    {
+        parent::__construct();
+        $this->container = $container;
+        $this->group = $group;
+        $this->user = $user;
+    }
+
+    public function matches($other)
+    {
+        if (!is_string($other)) {
+            return false;
+        }
+
+        try {
+            return sprintf('%s:%s', $this->group, $this->user) ===
+                $this->exec(sprintf('docker exec %s stat -c "%%G:%%U" %s', $this->container, $other));
+        } catch (\RuntimeException $e) {
+            return false;
+        }
+    }
+
+    private function exec(string $command) : string
+    {
+        exec($command, $output, $exitCode);
+
+        if ($exitCode > 0) {
+            throw new \RuntimeException('Command failed with exit code: ' . $exitCode);
+        }
+        
+        return $output;
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return 'has correct group and user in container ' . $this->container;
+    }
+}
---
+++ test/Test/Constraint/FileGroupAndOwnerInContainerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Jh\WorkflowTest\Test\Constraint;
+
+use Jh\Workflow\Test\Constraint\FileGroupAndOwnerInContainer;
+use phpmock\phpunit\PHPMock;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ * @runTestsInSeparateProcesses Because you cannot mock global functions which already used
+ */
+class FileGroupAndOwnerInContainerTest extends TestCase
+{
+    use PHPMock;
+
+    public function testMatchesWhenFileUserAndGroupCorrect()
+    {
+        $exec = $this->getFunctionMock('Jh\Workflow\Test\Constraint', 'exec');
+        $exec->expects($this->once())->willReturnCallback(
+            function ($command, &$output, &$exitCode) {
+                self::assertEquals('docker exec m2-php stat -c "%G:%U" some-file.txt', $command);
+                $exitCode = 0;
+                $output = 'www-data:www-data';
+            }
+        );
+
+        self::assertTrue(
+            (new FileGroupAndOwnerInContainer('m2-php', 'www-data', 'www-data'))->matches('some-file.txt')
+        );
+    }
+
+    public function testMatchesWhenFileUserAndGroupIncorrect()
+    {
+        $exec = $this->getFunctionMock('Jh\Workflow\Test\Constraint', 'exec');
+        $exec->expects($this->once())->willReturnCallback(
+            function ($command, &$output, &$exitCode) {
+                self::assertEquals('docker exec m2-php stat -c "%G:%U" some-file.txt', $command);
+                $exitCode = 0;
+                $output = 'root:root';
+            }
+        );
+
+        self::assertFalse(
+            (new FileGroupAndOwnerInContainer('m2-php', 'www-data', 'www-data'))->matches('some-file.txt')
+        );
+    }
+
+    public function testMatchesWhenFileDoesNotExist()
+    {
+        $exec = $this->getFunctionMock('Jh\Workflow\Test\Constraint', 'exec');
+        $exec->expects($this->once())->willReturnCallback(
+            function ($command, &$output, &$exitCode) {
+                self::assertEquals('docker exec m2-php stat -c "%G:%U" some-file.txt', $command);
+                $exitCode = 1;
+            }
+        );
+
+        self::assertFalse(
+            (new FileGroupAndOwnerInContainer('m2-php', 'www-data', 'www-data'))->matches('some-file.txt')
+        );
+    }
+
+    public function testToString()
+    {
+        self::assertEquals(
+            'has correct group and user in container m2-php',
+            (new FileGroupAndOwnerInContainer('m2-php', 'www-data', 'www-data'))->toString()
+        );
+    }
+}
