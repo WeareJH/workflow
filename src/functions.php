f2fb7c06f2eea6aabf863de6414a9bf8aa28ba6b
Aydin Hassan <aydin@hotmail.co.uk>
2017-08-07 09:30:05 +0200

    Remove function

--- src/functions.php
+++ src/functions.php
@@ -8,13 +8,3 @@ function toMap($function)
         return $function($item);
     };
 }
-
-function runProcessShowingOutput($command, $workingDirectory = null) : string
-{
-    global $c;
-    $output = $c->get(\Symfony\Component\Console\Output\OutputInterface::class);
-
-    return $c->get(ProcessFactory::class)->runSynchronous($command, $workingDirectory, function ($type, $buffer) use ($output) {
-        $output->writeLine($buffer);
-    });
-}
